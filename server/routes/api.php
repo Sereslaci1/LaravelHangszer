41451e7b1b357ddcfb6e54b50fa459443b6af40c
Sereslaci1 <laszloseres38@gmail.com>
2025-11-13 12:03:00 +0100

    crud kesz

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\InstrumentsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,3 +12,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function(){
     return 'API';
 });
+
+Route::get('instruments', [InstrumentsController::class, 'index']);
+Route::get('instruments/{id}', [InstrumentsController::class, 'show']);
+Route::post('instruments', [InstrumentsController::class, 'store']);
+Route::delete('instruments/{id}', [InstrumentsController::class, 'destroy']);
+Route::patch('instruments/{id}', [InstrumentsController::class, 'update']);
